7399605167024fb0678f042d6d16a5870028d62e
Kareem Khazem <ec09414@eecs.qmul.ac.uk>
2012-08-10 15:35:07 +0100

    changed package of qrlabeller to reflect folder structiore

--- label/QrLabeller.java
+++ label/QrLabeller.java
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package label;
 import javax.imageio.*;
 import java.io.*;
 import javax.swing.*;
