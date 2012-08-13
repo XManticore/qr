dfd4b98ed92a5b9087505490e88037dc4a1fc91d
Kareem Khazem <ec09414@eecs.qmul.ac.uk>
2012-08-13 11:56:31 +0100

    added label keys enum to simplify implementation

---
+++ label/LabelKeys.java
@@ -0,0 +1,22 @@
+/** Specifies fields that are useful in laying out a label. This enum
+ * describes the fields of the XML file that are useful for laying out a
+ * label. The XML file is described by pageLayout.dtd 
+ */
+public enum LabelKeys{
+  /** The height of the label, in pixels */
+  HEIGHT,
+  /** The width of the label, in pixels */
+  WIDTH,
+  /** The size of the QR code, in pixels*/
+  QR_SIZE,
+  /** The horizontal postion of th qr code on the label*/
+  QR_X,
+  /** The vertical postion of th qr code on the label*/
+  QR_Y,
+  /** The size of the text, in pt*/
+  TEXT_SIZE,
+  /** The horizontal postion of th text on the label*/
+  TEXT_X,
+  /** The vertical postion of th text on the label*/
+  TEXT_Y
+}
